674d644d779fe287a4358517c0625d8b0fee08eb
EmmaRinalduzzi <61381188+EmmaRinalduzzi@users.noreply.github.com>
2020-04-01 14:49:18 +0200

    Le personnage ne peut plus sortir de l'écran en x

--- Essais_Pygame/Essais Pygame/test_pygame.py
+++ Essais_Pygame/Essais Pygame/test_pygame.py
@@ -11,7 +11,7 @@ pygame.display.set_caption(TITLE)
 ## Gestion de la musique ####################################
 pygame.mixer.pre_init(44100, 4096)
 pygame.mixer.music.load("music.mp3")
-pygame.mixer.music.set_volume(0)
+pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
   ################################ on fait l'animation du perso
 #  walkright = pygame.image.load('personnage0D.png'), pygame.image.load('personnage1D.png'),pygame.image.load('personnage2D.png'),pygame.image.load('personnage3D.png'),pygame.image.load('personnage4D.png')
@@ -47,8 +47,8 @@ setBackgroundImage("image.jpg")
 ############################################################
 ## Initialisation du perso #################################
 #perso = pygame.image.load('perso.png').convert_alpha()
-x = 320
-y = 320
+PersoPosx = 320
+PersoPosy = 320
 ###########################################################
 
 # setBackgroundColour("pink")
@@ -58,31 +58,36 @@ showSprite(PersoSprite)
 nextFrame = clock()
 frame = 0
 while True:
- 
-
-    #pygame.display.flip()
-    if clock() > nextFrame:                         # We only animate our character every 80ms.
-        frame = (frame+1)%8                         # There are 8 frames of animation in each direction
-        nextFrame += 80                             # so the modulus 8 allows it to loop
+    if clock() > nextFrame:                         
+        frame = (frame+1)%8                         
+        nextFrame += 80                             
 
     if keyPressed("right"):
-        changeSpriteImage(PersoSprite, 0*8+frame)    # 0*8 because right animations are the 0th set in the sprite sheet
-        x += 5
+        changeSpriteImage(PersoSprite, 0*8+frame)    
+        PersoPosx += 5
     elif keyPressed("down"):
-        changeSpriteImage(PersoSprite, 1*8+frame)    # down facing animations are the 1st set
-        y += 5
+        changeSpriteImage(PersoSprite, 1*8+frame)   
+        PersoPosy += 5
     elif keyPressed("left"):
-        changeSpriteImage(PersoSprite, 2*8+frame)    # and so on
-        x -= 5
+        changeSpriteImage(PersoSprite, 2*8+frame)    
+        PersoPosx -= 5
     elif keyPressed("up"):
         changeSpriteImage(PersoSprite, 3*8+frame)
-        y -= 5
+        PersoPosy -= 5
+    elif keyPressed("escape"):
+        pygame.quit()
     else:
-        changeSpriteImage(PersoSprite, 1 * 8 + 5)  # the static facing front look
-    
+        changeSpriteImage(PersoSprite, 1 * 8 + 5)  
+   
+    if PersoPosx >= 640 :
+        PersoPosx = 640
+    if PersoPosx <=30:
+        PersoPosx = 30
+
+
     #fenetre.blit(PersoSprite, (x, y))
     #pygame.display.flip()
-    moveSprite(PersoSprite,x,y,True)
+    moveSprite(PersoSprite,PersoPosx,PersoPosy,True)
     # fenetre.fill(pygame.Color("#FF0000"))
     #fenetre.blit(texte, rectTexte)
   
