ce1da702b8109fb059de1567a2c3414f413cade7
aisrnd01 <62541322+aisrnd01@users.noreply.github.com>
2020-04-01 16:24:43 +0200

    coucou

--- Essais_Pygame/Essais Pygame/test_pygame.py
+++ Essais_Pygame/Essais Pygame/test_pygame.py
@@ -30,7 +30,7 @@ pygame.mixer.music.play(-1)
   
 screenSize(640, 480)
 PersoSprite  = makeSprite("links.gif", 32)  # links.gif contains 32 separate frames of animation.
-setBackgroundImage("image.jpg")
+setBackgroundImage("image.png")
 moveSprite(PersoSprite,300,300,True)
 showSprite(PersoSprite)
 
