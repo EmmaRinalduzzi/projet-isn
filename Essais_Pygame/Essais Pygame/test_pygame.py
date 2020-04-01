aeddbabe04a31c7d7faffa33e025121a107acf8a
EmmaRinalduzzi <61381188+EmmaRinalduzzi@users.noreply.github.com>
2020-04-01 15:04:58 +0200

    nettoyage

--- Essais_Pygame/Essais Pygame/test_pygame.py
+++ Essais_Pygame/Essais Pygame/test_pygame.py
@@ -1,4 +1,15 @@
 ## Initialisation ###########################################
+
+
+def LimiteScreen(x,y):
+    if x >= 640 :
+        x = 640
+    if x <=30:
+        x = 30
+    if y >= 450:
+        y = 450
+    return (x,y)
+
 import pygame
 from pygame.locals import *
 from pygame_functions import * 
@@ -6,6 +17,9 @@ pygame.init()
 fenetre = pygame.display.set_mode((640, 480))
 TITLE = "message emma"
 pygame.display.set_caption(TITLE)
+
+PersoPosx = 320
+PersoPosy = 320
 #rectScreen = fenetre.get_rect()  # coordonnée du rectangle de l'écran
 #############################################################
 ## Gestion de la musique ####################################
@@ -13,45 +27,10 @@ pygame.mixer.pre_init(44100, 4096)
 pygame.mixer.music.load("music.mp3")
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
-  ################################ on fait l'animation du perso
-#  walkright = pygame.image.load('personnage0D.png'), pygame.image.load('personnage1D.png'),pygame.image.load('personnage2D.png'),pygame.image.load('personnage3D.png'),pygame.image.load('personnage4D.png')
- # walkleft = pygame.image.load('personnage0G.png'),pygame.image.load('personnage1G.png'),pygame.image.load('personnage2G.png'),pygame.image.load('personnage3G.png'),pygame.image.load('personnage4G.png'),
-# agent = makeSprite("personnage0D.jpg")
-# addSpriteImage (agent, "personnage0D.jpg")
-# addSpriteImage (agent, "personnage1D.jpg")
-# addSpriteImage (agent, "personnage2D.jpg")
-# addSpriteImage (agent, "personnage3D.jpg")
-# addSpriteImage (agent, "personnage4D.jpg")
-# addSpriteImage (agent, "alaid.jpg")
-# addSpriteImage (agent, "personnage1G.jpg")
-# addSpriteImage (agent, "personnage2G.jpg")
-# addSpriteImage (agent, "personnage3G.jpg")
-# addSpriteImage (agent, "personnage4G.jpg")
-#pygame.sprite
-
+  
 screenSize(640, 480)
 PersoSprite  = makeSprite("links.gif", 32)  # links.gif contains 32 separate frames of animation.
 setBackgroundImage("image.jpg")
-
-#################################### Gestion de la police####   
-# police = pygame.font.Font(None, 24)
-# texte = police.render(
-#     "Flèche du haut pour monter, bouton gauche pour placer ou on veut", True, pygame.Color("#FFFF00"))
-# rectTexte = texte.get_rect()
-# donne au centre du rectangle contenant le texte, les coordonnées du centre de l'écran
-#rectTexte.center = rectScreen.center
-
-#############################################################
-## On pose le FondSprite ##########################################
-
-############################################################
-## Initialisation du perso #################################
-#perso = pygame.image.load('perso.png').convert_alpha()
-PersoPosx = 320
-PersoPosy = 320
-###########################################################
-
-# setBackgroundColour("pink")
 moveSprite(PersoSprite,300,300,True)
 showSprite(PersoSprite)
 
@@ -79,17 +58,11 @@ while True:
     else:
         changeSpriteImage(PersoSprite, 1 * 8 + 5)  
    
-    if PersoPosx >= 640 :
-        PersoPosx = 640
-    if PersoPosx <=30:
-        PersoPosx = 30
 
-
-    #fenetre.blit(PersoSprite, (x, y))
-    #pygame.display.flip()
+    TableauPositions = LimiteScreen(PersoPosx,PersoPosy) #verifier si le joueur ne sort pas de l'écran
+    PersoPosx = TableauPositions[0]
+    PersoPosy = TableauPositions[1]
     moveSprite(PersoSprite,PersoPosx,PersoPosy,True)
-    # fenetre.fill(pygame.Color("#FF0000"))
-    #fenetre.blit(texte, rectTexte)
   
     tick(120)
 
